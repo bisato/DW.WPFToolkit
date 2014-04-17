a691b7991d58bb6f8ec86a6052c1698a0aa6421a
David W <david.w.dev@gmail.com>
2014-04-17 17:40:20 +0200

    Take care how the license dialog is shown

--- DW.WPFToolkit/Internal/License.xaml.cs
+++ DW.WPFToolkit/Internal/License.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows;
 using System.Windows.Navigation;
 
 namespace License1
@@ -12,6 +15,7 @@ namespace License1
 
         private static bool _alreadyShown;
 
+        [STAThread]
         internal static void Display()
         {
             if (_alreadyShown)
@@ -19,14 +23,70 @@ namespace License1
 
             _alreadyShown = true;
 
-            var window = new License();
-            window.ShowDialog();
+            if (TryShowNormally())
+                return;
+
+            if (TryShowInThread())
+                return;
+
+            ShowFallback();
+        }
+
+        private static bool TryShowNormally()
+        {
+            try
+            {
+                var w = new License();
+                w.Show();
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private static bool TryShowInThread()
+        {
+            try
+            {
+                var thread = new Thread(() =>
+                {
+                    var w = new License();
+                    w.SizeToContent = SizeToContent.Manual;
+                    w.Height = 170;
+                    w.Show();
+                    System.Windows.Threading.Dispatcher.Run();
+                });
+                thread.SetApartmentState(ApartmentState.STA);
+                thread.IsBackground = true;
+                thread.Start();
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private static void ShowFallback()
+        {
+            try
+            {
+                System.Windows.Forms.MessageBox.Show("This is the Demo of the my-libraries DW.CodedUI 2.0 Package." + Environment.NewLine + "To order a full version visit: www.my-libraries.com");
+            }
+            catch
+            {
+                throw new InvalidOperationException("You try to run the DW.CodedUI demo but the license dialog cannot shown. In this case the demo is not working for you. Visit www.my-libraries.com to order a full license.");
+            }
         }
 
         private void ShowInBrowser(object sender, RequestNavigateEventArgs e)
         {
             Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
             e.Handled = true;
+
+            Close();
         }
     }
 }
